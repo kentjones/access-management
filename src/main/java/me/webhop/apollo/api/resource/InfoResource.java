d42062af295f3694b354f031eb7165bcdb231765
Kent Jones <jones.kent@pm.me>
2021-11-23 21:13:31 -0800

    return error message

--- src/main/java/me/webhop/apollo/api/resource/InfoResource.java
+++ src/main/java/me/webhop/apollo/api/resource/InfoResource.java
@@ -4,15 +4,17 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import me.webhop.apollo.model.ErrorResponse;
 
 @Path("info")
 public class InfoResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getIt()
+    public ErrorResponse getIt()
     {
-        return "Got it! It's working.";
+        return new ErrorResponse.ErrorResponseBuilder(201, "Got it! It's working.").build();
+
     }
 
 }
