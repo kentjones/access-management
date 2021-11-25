2a7db2c31e0ee4b9f2c573b9caabdb3fbbc2631e
Kent Jones <jones.kent@pm.me>
2021-11-25 10:23:23 -0800

    setting up logging

--- src/main/java/me/webhop/apollo/ApplicationConfig.java
+++ src/main/java/me/webhop/apollo/ApplicationConfig.java
@@ -1,23 +1,23 @@
 package me.webhop.apollo;
 
-import org.apache.log4j.Level;
 import org.glassfish.jersey.server.ResourceConfig;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import jakarta.ws.rs.ApplicationPath;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 @ApplicationPath("api")
 public class ApplicationConfig extends ResourceConfig {
 
-    private final Logger logger = Logger.getLogger(ApplicationConfig.class);
+    private final Logger logger = LoggerFactory.getLogger(ApplicationConfig.class);
     public ApplicationConfig() {
 
         packages("me.webhop.apollo.api", "me.webhop.apollo.security", "me.webhop.apollo.model", "com.fasterxml.jaxrs.json");
 
         register(JacksonJsonProvider.class);
 
-        logger.log(Level.INFO, "Registering web resources");
+        logger.info("Registering web resources");
     }
 }
