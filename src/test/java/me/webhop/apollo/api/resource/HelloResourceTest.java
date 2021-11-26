d8abf484411096f5a20e174a5f2626c5e9d90db1
Kent Jones <jones.kent@pm.me>
2021-11-25 23:00:08 -0800

    set up logging

--- src/test/java/me/webhop/apollo/api/resource/HelloResourceTest.java
+++ src/test/java/me/webhop/apollo/api/resource/HelloResourceTest.java
@@ -5,15 +5,22 @@ import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloResourceTest {
-
-    private WebTarget target;
-
+    private Logger logger = LoggerFactory.getLogger(HelloResourceTest.class);
+    private String name;
     @Before
     public void setup() throws Exception {
-
+        name = "Nite Coder";
     }
-
+    @Test
+    public void verifyLogback(){
+        logger.info("Hi there coming to you from {}",name);
+        Assert.assertTrue(true);
+    }
 }
