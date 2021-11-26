fb7c98d498cc54cc586b4637886909275548d617
Kent Jones <jones.kent@pm.me>
2021-11-26 08:16:46 -0800

    set up logging

--- src/main/java/me/webhop/apollo/util/RequestContextHelper.java
+++ src/main/java/me/webhop/apollo/util/RequestContextHelper.java
@@ -3,6 +3,7 @@ package me.webhop.apollo.util;
 import com.google.gson.Gson;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.core.Response;
+import static jakarta.ws.rs.core.Response.Status;
 import me.webhop.apollo.model.ErrorResponse;
 
 public class RequestContextHelper {
@@ -13,21 +14,21 @@ public class RequestContextHelper {
     {
 
         context.abortWith(Response
-                .status(Response.Status.FORBIDDEN)
+                .status(Status.FORBIDDEN)
                 .entity(gson.toJson(er))
                 .build());
     }
     public static void abortWith401Unauthorized(ContainerRequestContext context, ErrorResponse er)
     {
         context.abortWith(Response
-                .status(Response.Status.UNAUTHORIZED)
+                .status(Status.UNAUTHORIZED)
                 .entity(gson.toJson(er))
                 .build());
     }
     public static void abortWith500InternalServerError(ContainerRequestContext context, ErrorResponse er)
     {
         context.abortWith(Response
-                .status(Response.Status.INTERNAL_SERVER_ERROR)
+                .status(Status.INTERNAL_SERVER_ERROR)
                 .entity(gson.toJson(er))
                 .build());
     }
